c0885cb4b484bbc1ce8a2313add60298299711b0
Weilin.Yuan <61404388+onecoinbuybus@users.noreply.github.com>
2023-09-14 22:02:35 +0900

    Update poly.py

--- poly.py
+++ poly.py
@@ -16,7 +16,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, Descriptors
 from rdkit import Geometry as Geom
 from rdkit import RDLogger
-import calc, const, utils
+from . import calc, const, utils
 
 __version__ = '0.2.6'
 
